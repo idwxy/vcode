a97119f9a90c7fc0a5e730853493f904fc5cb54d
idwxy <idwxy@live.com>
2019-11-01 16:59:50 +0800

    pro commit

---
+++ src/main/java/com/idwxy/vcode/VcodeApplication.java
@@ -0,0 +1,13 @@
+package com.idwxy.vcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcodeApplication.class, args);
+	}
+
+}
